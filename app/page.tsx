a76701b6781bc8b3cfdb2e2e19e8257b8da71ae3
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-12-26 12:38:44 +0100

    Implement container component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div className="h-screen flex items-center justify-center text-5xl font-semibold text-primary bg-crystalVeil">
-      <h1>Hello NEXTJS!</h1>
+      <h1>
+        Hello from <span className="text-abyssalNightfall">Lulu!</span>
+      </h1>
     </div>
   );
 };
